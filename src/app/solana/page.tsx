68fddcdf76cf28176d837b8b2a054cd6fcff815c
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-11 22:38:32 +0530

    create the solana route for user to verify owner ship of solana pub keys across wallets

---
+++ src/app/solana/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { WalletProvider } from '@solana/wallet-adapter-react';
+
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+
+const WalletDisconnectButtonDynamic = dynamic(
+  async () =>
+    (await import('@solana/wallet-adapter-react-ui')).WalletDisconnectButton,
+  { ssr: false }
+);
+const WalletMultiButtonDynamic = dynamic(
+  async () =>
+    (await import('@solana/wallet-adapter-react-ui')).WalletMultiButton,
+  { ssr: false }
+);
+
+import '@solana/wallet-adapter-react-ui/styles.css';
+import VerifySolAddressOwnership from '../components/VerifySolAddressOwnership';
+import dynamic from 'next/dynamic';
+
+export default function Page() {
+  return (
+    <div>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButtonDynamic />
+          <WalletDisconnectButtonDynamic />
+          <VerifySolAddressOwnership />
+        </WalletModalProvider>
+      </WalletProvider>
+    </div>
+  );
+}

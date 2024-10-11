7832a72b9da8df8d720c2e8d4d591a94091df5ae
Harsimranjit Dhaliwal <harsimranyadu@gmail.com>
2024-10-11 23:06:19 +0530

    add link to home page to solana route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Welcome to Addr Verifier</div>;
+  return (
+    <div className="flex flex-col m-auto w-72">
+      <div>Welcome to Addr Verifier</div>
+      <p>
+        To verify with{' '}
+        <Link className="text-blue-600 underline" href={'/solana'}>
+          Solana
+        </Link>
+      </p>
+    </div>
+  );
 }
